c3867ccb80d685552d642a5ff5fc0aff3346911e
Tanmay Nagwekar <tanmaynag26@gmail.com>
2025-10-30 20:31:30 +0530

    Express PostgreSQL Connection

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./config/db.js"
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5001;
 
 //Middlewares
 app.use(express.json())
-app.use(cors);
+app.use(cors());
 
 //Routes
 
 
 //Error handling middleware
+
+// Testing POSTGRES Connection
+app.get("/", async(req, res)=>{
+    const result = await pool.query("SELECT current_database()");
+    console.log("end");
+    res.send(`The database name is : ${result.rows[0].current_database}`);
+});
+
 app.listen(port,()=>{
     console.log(`Server is running on http:localhost:${port}`);
 });
